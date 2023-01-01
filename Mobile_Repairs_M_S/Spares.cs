00b8926b0a8c8429b637ec796a10de23f0813e77
ahmedkhaledhasan40@gmail.com <ahmedkhaledhasan40@gmail.com>
2022-12-31 16:37:02 -0800

    save button codeing is done in Spares form

--- Mobile_Repairs_M_S/Spares.cs
+++ Mobile_Repairs_M_S/Spares.cs
@@ -31,7 +31,29 @@ namespace Mobile_Repairs_M_S
         }
         private void SaveBtn_Click(object sender, EventArgs e)
         {
-
+            if (PartNameTb.Text == "" || PartCostTb.Text == "" )
+            {
+                MessageBox.Show("Messing Data !!!");
+            }
+            else
+            {
+                try
+                {
+                    string PName = PartNameTb.Text;
+                    int Cost = Convert.ToInt32(PartCostTb.Text);
+                    
+                    string Query = "insert into SpareTb1 values('{0}',{1})";
+                    Query = string.Format(Query, PName, Cost);
+                    Con.SetData(Query);
+                    MessageBox.Show("Spare Added!!!");
+                    ShowSpares();
+                    Clear();
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+            }
         }
     }
 }
